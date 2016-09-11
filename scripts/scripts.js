b5af7147c724dac9bde213d5e81100fb887c6f0f
Johanna Snow <johannadsnow@gmail.com>
2016-09-10 19:19:41 -0500

    Super Basic Skeleton

---
+++ scripts/scripts.js
@@ -0,0 +1,14 @@
+console.log ('This is sourced!')
+
+/// == Global Variable Declarations == ///
+
+
+/// == Function Declarations == ///
+
+
+/// == JavaScript == ///
+
+$(document).ready(function(){
+  console.log('Document ready!');
+
+}); // end document ready
