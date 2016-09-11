c8b3b65ced6ac5922a119ffedd80128c8577742e
Johanna Snow <johannadsnow@gmail.com>
2016-09-11 18:06:34 -0500

    functionalityneeded, yo

---
+++ public/scripts/clientscript.js
@@ -0,0 +1,59 @@
+console.log ('This is sourced!');
+
+//console.log('sourced');
+var calculation = '';
+
+
+$(document).ready(function(){
+  console.log('Document ready!');
+
+  //console.log('sourced');
+  var calculation = '';
+
+
+  //get calculator button input
+    $('button').on('click', function () {
+      if ($(this).attr('data') === "="){
+        returnResults();
+      }else if ($(this).attr('data') === 'CE') {
+        calculation = '';
+        $('.outputScreen').html(calculation);
+      }else{
+      //console.log('in button click');
+      var nextInput = $(this).attr('data');
+
+      //put in calculationIn array
+      calculationIn += nextInput;
+      console.log(calculation);
+      //display current calculation
+      $('.outputScreen').html(calculation);
+      }
+    });
+
+    var results = function () {
+      console.log('in results');
+      var calculationSent = {takeThis: calculation};
+      console.log(calculationSent);
+      $.ajax({
+        type: 'POST',
+        url: '/calculate',
+        data: calculationSent,
+        success: function (data) {
+          console.log(data);
+          var finalResults = data.takeThis;
+          console.log(finalResults);
+          $('.outputScreen').html(finalResults);
+        },
+        error: function (data) {
+          console.log('');
+          $('.outputScreen').html('Try again please');
+
+        }
+      });
+    };
+
+
+
+  });
+
+}); // end document ready
--- server/app.js
+++ server/app.js
@@ -8,15 +8,27 @@ var urlEncodedParser = bodyParser.urlencoded({extended:false});
 var portDecision = process.env.PORT || 3000;
 
 // spin up server
-app.listen(portDecision, function(){
+app.listen( portDecision, function(){
   console.log('Server is listening on heroku or port 3000');
-});
+}); //end server powering up
 
 // base url hit
 app.get('/', function(req,res){
   console.log('base url hit');
   res.sendFile(path.resolve('public/index.html'));
-});
+}); // end get
 
-// setup 'public' as a static resource
+//get requested calculation and calculate
+app.post( '/calculate', urlencodedParser, function ( req, res) {
+  //console.log('in post');
+  //console.log('this is req', req.body);
+  //calculate string
+  var finishedCalculation = eval(req.body.takeThis);
+  console.log('finishedCalculation', finishedCalculation);
+  //send back
+  var sendBack = {takeThis: finishedCalculation};
+  res.send(sendBack);
+  console.log(sendBack);
+});
+// setup 'public' as a static resource /// this means anyone can use it
 app.use(express.static('public'));
